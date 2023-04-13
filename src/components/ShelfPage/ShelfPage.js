10e1093c452a7a0002bfba2ca7763f55603b4e87
Adam Shepard <adam.w.gangl@gmail.com>
2023-04-13 14:54:03 -0500

    added input form and handleInputChange function and local state

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch, useEffect } from 'react';
+import { useState } from 'react';
 
 function ShelfPage() {
   const dispatch = useDispatch()
   const shelfItems = useSelector(store => store.itemReducer)
   console.log('this is shelfItems', shelfItems);
 
+  let [itemToAdd, setItemToAdd] = useState({ description: '', image_url: ''})
+
+console.log('itemToAdd', itemToAdd)
+
   useEffect(() => {
     dispatch({ type: 'FETCH_ITEMS' })
   }, []);
 
+const handleInputChange = (event) => {
+  const {name, value} = event.target;
+  setItemToAdd({
+    ...itemToAdd,
+    [name] : value,
+  })
+}
 
   return (
     <div className="container">
@@ -21,13 +33,28 @@ function ShelfPage() {
       {shelfItems ?
           shelfItems.map((item) => (
               <p key={item.id}>
+                <img src={item.image_url} width="300"/>
                 {item.description}
               </p>
           ))
         :
         (<p>Loading</p>)
       }
-      
+
+      <p>Add Form</p>
+      <input 
+      onChange={handleInputChange}
+      name="image_url"
+      value={itemToAdd.image_url}
+      type="text" 
+      placeholder="image url"></input>
+      <input 
+      onChange={handleInputChange}
+      name='description'
+      value={itemToAdd.description}
+      type="text" 
+      placeholder="description"></input>
+      <button>Add</button>
     </div>
   );
 }
