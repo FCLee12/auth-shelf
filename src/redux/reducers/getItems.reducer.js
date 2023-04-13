1a7579893d3546c46ca82d3dd265b5ee8970c88a
Siv Earley <searley96@gmail.com>
2023-04-13 13:25:53 -0500

    set up getItems.reducer

--- src/redux/reducers/getItems.reducer.js
+++ src/redux/reducers/getItems.reducer.js
@@ -1,5 +1,10 @@
 const itemReducer = (state = {}, action) => {
-    switch(action.type) {
-        case'
-    }
-}
+  switch (action.type) {
+    case "SET_ITEMS":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default itemReducer;
